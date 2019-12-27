b9ed6bd0d3e704a43b2e78ec0d6563aada74f2a4
lbenthins <lbenthins@gmail.com>
2019-12-27 15:17:13 +0100

    Added supported services

--- obd_service_test.py
+++ obd_service_test.py
@@ -1,5 +1,5 @@
 import isotp
-import time
+from my_obd import service
 
 CAN_INTERFACE = "vcan0"
 VEHICLE_SPEED = 0x1E
@@ -9,21 +9,23 @@ RX_ID = 0x7DF
 TX_ID = 0x7E8
 
 can_socket = isotp.socket()
+can_socket2 = isotp.socket()
 can_socket.bind(CAN_INTERFACE, isotp.Address(rxid=RX_ID, txid=TX_ID))
+can_socket2.bind(CAN_INTERFACE, isotp.Address(rxid=0x7E0, txid=TX_ID))
 
 
-def process_request(r):
-    print("Received bytes " + str(r.hex()))
-    if r[0] == int(0x01):
-        print("Service: Current Data")
-        if r[1] == int(0x0D):
-            print("PID: Vehicle Speed")
-            return bytes([POSITIVE_ANSWER + 0x01]) + bytes([0x0D]) + bytes([VEHICLE_SPEED])
+def process_request(req):
+    pids = service.get_service_pids(req[0])
+    if pids is not None:
+        return service.get_pid_response(pids, req[1])
+    return None
 
 
 while True:
     request = can_socket.recv()
     if request is not None:
         response = process_request(request)
-        print("Response: " + response.hex())
-    time.sleep(2)
+        if response is not None:
+            print("Response: " + response.hex())
+            can_socket2.send(response)
+
