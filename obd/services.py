38c609e37b4a0a5d8f4848d6f244a3bb5dacd53d
lbenthins <lbenthins@gmail.com>
2019-12-28 20:03:20 +0100

    Refactored services

--- obd/services.py
+++ obd/services.py
@@ -1,21 +1,30 @@
+BIG_ENDIAN = "big"
+
+SUPPORTED_PIDS_RESPONSE_MASK = 0x80000000
+
+SUPPORTED_PIDS_RESPONSE_INIT_VALUE = 0x00000001
+
+SUPPORTED_PIDS_RESPONSE_NUMBER_OF_PIDs = 32
+
+
 def get_vehicle_speed():
-    return 0x1E.to_bytes(1, byteorder="big")
+    return 0x1E.to_bytes(1, BIG_ENDIAN)
 
 
 def get_fuel_level():
-    return 0x10.to_bytes(1, byteorder="big")
+    return 0x10.to_bytes(1, BIG_ENDIAN)
 
 
 def get_engine_temperature():
-    return 0x20.to_bytes(1, byteorder="big")
+    return 0x20.to_bytes(1, BIG_ENDIAN)
 
 
 def get_fuel_type():
-    return 0x01.to_bytes(1, byteorder="big")
+    return 0x01.to_bytes(1, BIG_ENDIAN)
 
 
 SERVICES = [
-    {0x01: "Show current data",
+    {"id": 0x01, "description": "Show current data",
      "pids": [
          {"id": 0x05, "description": "Engine coolant temperature", "response": get_engine_temperature()},
          {"id": 0x0D, "description": "Vehicle speed", "response": get_vehicle_speed()},
@@ -25,36 +34,39 @@ SERVICES = [
 ]
 
 
-def get_service_pids(service_id):
+def process_service_request(requested_service, requested_pid):
+    if isinstance(requested_service, int) and isinstance(requested_pid, int):
+        pids = get_service_pids(requested_service)
+        if pids is not None:
+            if is_supported_pids_request(requested_pid):
+                return get_supported_pids_response(pids, requested_pid)
+            for pid in pids:
+                if pid.get("id") == requested_pid:
+                    return pid.get("response")
+        return None
+
+
+def get_service_pids(requested_service):
     for service in SERVICES:
-        if service.get(service_id) is not None:
+        if service.get("id") == requested_service:
             return service.get("pids")
     return None
 
 
+def is_supported_pids_request(requested_pid):
+    return requested_pid % SUPPORTED_PIDS_RESPONSE_NUMBER_OF_PIDs == 0
+
+
 def get_supported_pids_response(supported_pids, requested_pid):
-    supported_pids_response = 0x00000001
-    if requested_pid / 32 > 6:
-        supported_pids_response = 0x00000000
-    for p in supported_pids:
-        supported_pid = p.get("id")
-        if requested_pid < supported_pid < (requested_pid + 32):
-            supported_pids_response = supported_pids_response | 0x80000000 >> (supported_pid - requested_pid - 1)
-    return supported_pids_response.to_bytes(4, byteorder="big")
+    supported_pids_response = init_supported_pids_response(requested_pid)
+    for pid in supported_pids:
+        supported_pid = pid.get("id")
+        if requested_pid < supported_pid < (requested_pid + SUPPORTED_PIDS_RESPONSE_NUMBER_OF_PIDs):
+            supported_pids_response |= SUPPORTED_PIDS_RESPONSE_MASK >> (supported_pid - requested_pid - 1)
+    return supported_pids_response.to_bytes(4, BIG_ENDIAN)
 
 
-def is_supported_pids_request(pid):
-    if pid == 0x00:
-        return True
-    return pid % 32 == 0
-
-
-def process_service_request(requested_service, requested_pid):
-    pids = get_service_pids(requested_service)
-    if pids is not None:
-        if is_supported_pids_request(requested_pid):
-            return get_supported_pids_response(pids, requested_pid)
-        for pid in pids:
-            if pid.get("id") == requested_pid:
-                return pid.get("response")
-    return None
+def init_supported_pids_response(requested_pid):
+    if requested_pid / SUPPORTED_PIDS_RESPONSE_NUMBER_OF_PIDs > 6:
+        return SUPPORTED_PIDS_RESPONSE_INIT_VALUE >> 1
+    return SUPPORTED_PIDS_RESPONSE_INIT_VALUE
--- tests/test_obd/test_services.py
+++ tests/test_obd/test_services.py
@@ -1,28 +1,49 @@
 import unittest
 from obd import services
 
+INVALID_SERVICE_ID = "yy"
+
+INVALID_PID = "xx"
+
+UNKNOWN_PID = 0xFF
+
+UNKNOWN_SERVICE = 0xFF
+
 
 class TestServices(unittest.TestCase):
 
     def test_process_service_0x01_pid_0x00(self):
-        response = services.process_service_request(0x01, 0x00)
+        response = services.process_service_request(requested_service=0x01, requested_pid=0x00)
         self.assertIsNotNone(response)
         self.assertEqual("08080001", response.hex())
 
     def test_process_service_0x01_pid_0x20(self):
-        response = services.process_service_request(0x01, 0x20)
+        response = services.process_service_request(requested_service=0x01, requested_pid=0x20)
         self.assertIsNotNone(response)
         self.assertEqual("00020001", response.hex())
 
     def test_process_service_0x01_pid_0x40(self):
-        response = services.process_service_request(0x01, 0x40)
+        response = services.process_service_request(requested_service=0x01, requested_pid=0x40)
         self.assertIsNotNone(response)
         self.assertEqual("00008001", response.hex())
 
     def test_process_service_0x01_pid_0xE0_returns_no_pids(self):
-        response = services.process_service_request(0x01, 0xE0)
+        response = services.process_service_request(requested_service=0x01, requested_pid=0xE0)
         self.assertIsNotNone(response)
         self.assertEqual("00000000", response.hex())
+
+    def test_process_unknown_service_returns_none(self):
+        response = services.process_service_request(requested_service=UNKNOWN_SERVICE, requested_pid=0x00)
+        self.assertIsNone(response)
+
+    def test_process_service_unknown_pid_returns_none(self):
+        response = services.process_service_request(requested_service=0x01, requested_pid=UNKNOWN_PID)
+        self.assertIsNone(response)
+
+    def test_process_invalid_service_request_returns_none(self):
+        self.assertIsNone(services.process_service_request(requested_service=INVALID_SERVICE_ID, requested_pid=0x00))
+        self.assertIsNone(services.process_service_request(requested_service=0x01, requested_pid=INVALID_PID))
+        self.assertIsNone(services.process_service_request(requested_service=INVALID_SERVICE_ID, requested_pid=INVALID_PID))
 
 
 if __name__ == '__main__':
