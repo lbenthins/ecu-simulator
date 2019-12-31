c2c5d290e0ea3359a368ad430f19207e1d48dd5d
lbenthins <lbenthins@gmail.com>
2019-12-31 11:59:03 +0100

    test with lambda

--- obd/services.py
+++ obd/services.py
@@ -8,20 +8,27 @@ SUPPORTED_PIDS_RESPONSE_INIT_VALUE = 0x00000001
 
 SUPPORTED_PIDS_RESPONSE_NUMBER_OF_PIDs = 32
 
+FUEL_TYPE = ecu_info.get_fuel_type()
+
+DTCs = ecu_info.get_dtcs()
+
+VIN = ecu_info.get_vin()
+
+ECU_NAME = ecu_info.get_ecu_name()
 
 SERVICES = [
     {"id": 0x01, "description": "Show current data",
      "pids": [
-         {"id": 0x05, "description": "Engine coolant temperature", "response": ecu_info.get_engine_temperature()},
-         {"id": 0x0D, "description": "Vehicle speed", "response": ecu_info.get_vehicle_speed()},
-         {"id": 0x2F, "description": "Fuel tank level input", "response": ecu_info.get_fuel_level()},
-         {"id": 0x51, "description": "Fuel type", "response": ecu_info.get_fuel_type()}
+         {"id": 0x05, "description": "Engine coolant temperature", "response": lambda: ecu_info.get_engine_temperature()},
+         {"id": 0x0D, "description": "Vehicle speed", "response": lambda: ecu_info.get_vehicle_speed()},
+         {"id": 0x2F, "description": "Fuel tank level input", "response": lambda: ecu_info.get_fuel_level()},
+         {"id": 0x51, "description": "Fuel type", "response": lambda: FUEL_TYPE}
      ]},
-    {"id": 0x03, "description": "Show DTCs", "response": ecu_info.get_dtcs()},
+    {"id": 0x03, "description": "Show DTCs", "response": lambda: DTCs},
     {"id": 0x09, "description": "Request vehicle information",
      "pids": [
-         {"id": 0x02, "description": "Vehicle Identification Number(VIN)", "response": ecu_info.get_vin()},
-         {"id": 0x0A, "description": "ECU name", "response": ecu_info.get_ecu_name()}
+         {"id": 0x02, "description": "Vehicle Identification Number(VIN)", "response": lambda: VIN},
+         {"id": 0x0A, "description": "ECU name", "response": lambda: ECU_NAME}
      ]}
 ]
 
@@ -32,35 +39,28 @@ def process_service_request(requested_service, requested_pid):
         if service_pids is not None and requested_pid is not None:
             if is_supported_pids_request(requested_pid):
                 return get_supported_pids_response(service_pids, requested_pid)
-            return get_pid_response(requested_pid, service_pids)
-        return service_response
+            return get_pid_response(requested_pid, service_pids)()
+        return service_response()
     return None
 
 
 def is_service_request_valid(requested_service, requested_pid):
     return (isinstance(requested_service, int) and isinstance(requested_pid, int)) \
-            or (isinstance(requested_service, int) and requested_pid is None)
+           or (isinstance(requested_service, int) and requested_pid is None)
 
 
 def get_service(requested_service):
     for service in SERVICES:
         if service.get("id") == requested_service:
             return service.get("response"), service.get("pids")
-    return None, None
-
-
-def get_service_response(requested_service):
-    for service in SERVICES:
-        if service.get("id") == requested_service:
-            return service.get("response")
-    return None
+    return lambda: None, None
 
 
 def get_pid_response(requested_pid, pids):
     for pid in pids:
         if pid.get("id") == requested_pid:
             return pid.get("response")
-    return None
+    return lambda: None
 
 
 def is_supported_pids_request(requested_pid):
